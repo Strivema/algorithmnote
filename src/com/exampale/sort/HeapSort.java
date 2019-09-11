dd2565bada204a17cd2e05d1def4ab2794af787f
ray.ma <ray.ma@chinaentropy.com>
2019-09-11 11:24:05 +0800

    堆排序

---
+++ src/com/exampale/sort/HeapSort.java
@@ -0,0 +1,42 @@
+package com.exampale.sort;
+
+import static com.exampale.sort.QuickSort.swap;
+
+/**
+ * @author Ray.Ma
+ * @date 2019/9/6 11:08
+ */
+public class HeapSort {
+    public static void heapInsert(int[] arr, int index) {
+        while (arr[index] > arr[(index - 1) / 2]) {
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    public static void heapify(int[] arr, int index, int size) {
+        int left = index * 2 + 1;
+        while (left < size) {
+            int largest = left + 1 < size && arr[left + 1] > arr[left] ? left + 1 : left;
+            largest = arr[largest] > arr[index] ? largest : index;
+            if (largest == index) {
+                break;
+            }
+            swap(arr, largest, index);
+            index = largest;
+            left = index * 2 + 1;
+        }
+    }
+
+    public static void heapSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            heapInsert(arr, i);
+        }
+        int size = arr.length;
+        swap(arr, 0, --size);
+        while (size > 0) {
+            heapify(arr, 0, size);
+            swap(arr, 0, --size);
+        }
+    }
+}
