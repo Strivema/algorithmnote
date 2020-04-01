03f8109a363f5185ddcc8b2b31f6659fe6f86557
marui <markmarui@vip.qq.com>
2020-04-01 23:27:11 +0800

    删除最外层的括号

---
+++ src/com/exampale/leetcode/easy/RemoveOuterParentheses.java
@@ -0,0 +1,48 @@
+package com.exampale.leetcode.easy;
+
+import java.util.Stack;
+
+/**
+ * @author Marie
+ * @date 2020/4/1 11:08 PM
+ **/
+public class RemoveOuterParentheses {
+    /**
+     * 有效括号字符串为空 ("")、"(" + A + ")" 或 A + B，其中 A 和 B 都是有效的括号字符串
+     * ，+ 代表字符串的连接。例如，""，"()"，"(())()" 和 "(()(()))" 都是有效的括号字符串。
+     *
+     * @param S
+     * @return
+     */
+    public String removeOuterParentheses(String S) {
+        StringBuilder res = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        int start = 0;
+        int end = 0;
+        boolean flag = false;
+        char ch;
+        for (int i = 0; i < S.length(); i++) {
+            ch = S.charAt(i);
+            if (ch == '(') {
+                stack.push(ch);
+                if (!flag) {
+                    start = i;
+                    flag = true;
+                }
+
+            }
+            if (ch == ')') {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    end = i;
+                    res.append(S.substring(start + 1, end));
+                    flag = false;
+                    start = end;
+                }
+            }
+        }
+        return res.toString();
+
+    }
+
+}
