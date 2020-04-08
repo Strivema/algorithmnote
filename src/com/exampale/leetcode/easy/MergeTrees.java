b33e9639c64ec295eb8c5f6480f7f579ac49a5f8
marui <markmarui@vip.qq.com>
2020-04-08 23:07:04 +0800

    合并二叉树

--- src/com/exampale/leetcode/easy/MergeTrees.java
+++ src/com/exampale/leetcode/easy/MergeTrees.java
@@ -1,5 +1,7 @@
 package com.exampale.leetcode.easy;
 
+import java.util.Stack;
+
 /**
  * @author Marie
  * @date 2020/4/8 10:40 PM
@@ -9,6 +11,8 @@ public class MergeTrees {
      * 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
      * <p>
      * 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+     * <p>
+     * 方法一：递归
      *
      * @param t1
      * @param t2
@@ -26,5 +30,40 @@ public class MergeTrees {
         return t1;
 
     }
+
+    /**
+     * 方法二：
+     *
+     * @param t1
+     * @param t2
+     * @return
+     */
+
+    public TreeNode mergeTrees1(TreeNode t1, TreeNode t2) {
+        if (t1 == null)
+            return t2;
+        Stack<TreeNode[]> stack = new Stack<>();
+        stack.push(new TreeNode[]{t1, t2});
+        while (!stack.isEmpty()) {
+            TreeNode temp[] = stack.pop();
+            if (temp[0] == null || temp[1] == null) {
+                continue;
+            }
+            temp[0].val += temp[1].val;
+            if (temp[0].left == null) {
+                temp[0].left = temp[1].left;
+            } else {
+                stack.push(new TreeNode[]{temp[0].left, temp[0].left});
+            }
+            if (temp[0].right == null) {
+                temp[0].right = temp[1].right;
+            } else {
+                stack.push(new TreeNode[]{temp[0].right, temp[1].right});
+            }
+        }
+        return t1;
+
+    }
+
 
 }
